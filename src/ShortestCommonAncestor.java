077ca6ca701a5c68b451f66fb3f9cf4306e9ef96
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-04-26 10:32:06 -0500

    Draft of constructor and ancestor methods
    Questions: what defines a rooted DAG?

--- src/ShortestCommonAncestor.java
+++ src/ShortestCommonAncestor.java
@@ -1,55 +1,81 @@
+import java.util.*;
+
 public class ShortestCommonAncestor {
+    
+    Digraph graph; 
+    int V;
+    int root; 
+    
+    //constructor method takes a rooted DAG as an argument
+    public ShortestCommonAncestor(Digraph G){
+        
+        //validate that argument is a DAG 
+        DirectedCycle cycle=new DirectedCycle(G);
+        if(cycle.hasCycle()) throw new IllegalArgumentException("Digraph is not a DAG");
+        
+        //Step 2: is it rooted? Do we need to validate that it's rooted? 
+        graph=new Digraph(G);
+        V=graph.V();
+        root=0;
+        boolean rooted=false; 
+        
+        while(root<V && !rooted){
+            DirectedDFS search=new DirectedDFS(graph.reverse(), root);
+            if(search.count()==V)
+                rooted=true;
+        }  
+        /*
+        System.out.println("DAG is rooted: "+rooted);
+        System.out.println("Root vertex: "+root);
+        DirectedDFS search=new DirectedDFS(graph.reverse(), root);
+        for(int i=0; i<search.count(); i++){
+            System.out.println(i+"  "+search.marked(i));
+        }*/
 
-	/**
-	 * constructor takes a rooted DAG as argument
-	 * @param G
-	 */
-	public ShortestCommonAncestor(Digraph G) {
-
-	}
-
-	/**
-	 * length of shortest ancestral path between v and w. 
-	 * Maybe use lengthSubset and/or ancestorSubset in this function, idk for sure though
-	 * @param v
-	 * @param w
-	 * @return
-	 */
-	public int length(int v, int w) {
-
-	}
-
-	/**
-	 * a shortest common ancestor of vertices v and w
-	 * @param v
-	 * @param w
-	 * @return
-	 */
-	public int ancestor(int v, int w) {
-
-	}
-
-	/** length of shortest ancestral path of vertex subsets A and B
-	 * @param subsetA
-	 * @param subsetB
-	 * @return
-	 */
-	public int lengthSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
-
-	}
-
-	/**
-	 * a shortest common ancestor of vertex subsets A and B
-	 * @param subsetA
-	 * @param subsetB
-	 * @return
-	 */
-	public int ancestorSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
-
-	}
-
-	// test this class out and make sure it works. Leave code in here when done cus it's required now
-	public static void main(String[] args) {
-
-	}
+    }
+    
+    //length of shortest ancestral path between v and w
+    public int length(int v, int w){
+        
+        return 0;
+    }
+    
+    //a shortest common ancestor of vertices v and w
+    public int ancestor(int v, int w){
+        
+        ArrayList<Integer> commonAncestors=new ArrayList<Integer>();
+        //find vertices reachable from v
+        DirectedDFS searchV=new DirectedDFS(graph, v);
+        //find vertices reachable from w 
+        DirectedDFS searchW=new DirectedDFS(graph, w);
+        //determine which vertices are reachable from both v and w 
+        for(int i=0; i<V; i++){
+            if(searchV.marked(i) && searchW.marked(i))
+                commonAncestors.add(i);
+        }
+        return commonAncestors.get(commonAncestors.size());
+    }
+    
+    //length of shortest ancestral path of vertex subsets A and B
+    public int lengthSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB){
+        
+        return 0;
+    }
+    
+    //a shortest common ancestor of vertex subsets A and B
+    public int ancestorSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB){
+        
+        ArrayList<Integer> commonAncestors=new ArrayList<Integer>();
+        //find vertices reachable from v
+        DirectedDFS searchA=new DirectedDFS(graph, subsetA);
+        //find vertices reachable from w 
+        DirectedDFS searchB=new DirectedDFS(graph, subsetB);
+        //determine which vertices are reachable from both v and w 
+        for(int i=0; i<V; i++){
+            if(searchA.marked(i) && searchB.marked(i))
+                commonAncestors.add(i);
+        }
+        return commonAncestors.get(commonAncestors.size()); //don't actually return this tho
+    }   
+    
 }
