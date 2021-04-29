1a5084901680eed398c871f35ec489e922d3b1b5
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-04-28 20:19:06 -0500

    Final Outcast class

--- src/Outcast.java
+++ src/Outcast.java
@@ -1,24 +1,64 @@
+import java.io.*;
+import java.util.*;
+
 public class Outcast {
-
-	/**
-	 * an outcast is the noun that is the least related to the others
-	 * in a set of WordNet nouns. See assignment for how to identify
-	 * an outcast
-	 */
-
-	// constructor
-	public Outcast(WordNet wn) {
-
-	}
-
-	// given an array of WordNet nouns, return an outcast
-	public String outcast(String[] nouns) {
-
-	}
-
-	// test the class. Required to leave code in here when done -- see assignment
-	public static void main(String[] args) {
-
-	}
-
+    
+    private WordNet words;
+    
+    //constructor takes WordNet object
+    public Outcast(WordNet wordnet){
+        words=wordnet;
+        
+    }
+    
+    //given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns){
+        
+        //calculate and store the sum of the distances for each noun
+        int[] distances=new int[nouns.length];
+        for(int i=0; i<nouns.length; i++){
+            distances[i]=0;
+            for(int j=0; j<nouns.length; j++)
+                distances[i]+=words.distance(nouns[i],nouns[j]);
+        }
+        
+        //determine index of noun with the maximum sum of distances
+        int max=0;
+        int maxIndex=0;
+        for(int i=0; i<nouns.length; i++){
+            if(distances[i]>max){
+                maxIndex=i;
+                max=distances[i];
+            }
+        }
+        
+        //return outcast, i.e. noun indexed by maxIndex
+        return nouns[maxIndex];
+    }
+    
+    //test client takes names of synset, hypernym, and outcast files, and prints an outcast in each file
+    public static void main(String[] args) throws FileNotFoundException{
+        WordNet wordnet=new WordNet("synsets.txt", "hypernyms.txt");
+        Outcast outcast=new Outcast(wordnet);
+        
+        for(int t=0; t<3; t++){
+            Scanner input=new Scanner(System.in);
+            System.out.print("Enter the file name: ");
+            String fileName=input.nextLine();
+            System.out.print("\nEnter the file size: ");
+            int fileSize=input.nextInt();
+            
+            File inFile=new File(fileName);
+            Scanner input1=new Scanner(inFile);
+            String[] nouns=new String[fileSize];
+            
+            int i=0;
+            while(input1.hasNextLine()){
+                nouns[i]=input1.nextLine();
+                i++;
+            }
+            
+            System.out.println(fileName+": "+outcast.outcast(nouns));
+        }
+    }
 }
